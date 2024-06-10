2bf68e925967eaf80c02e1fb4a31dce18f1f354b
cashrina <khripunova.irina.24@gmail.com>
2024-06-10 17:00:44 +0600

    Добавлена кнопка закрытия модального окна

--- vite-project/src/components/Modal/Modal.tsx
+++ vite-project/src/components/Modal/Modal.tsx
@@ -16,15 +16,15 @@ const Modal: React.FC<ShowProps> = ({show,
             <div className="modal show" style={{display: show ? 'block' : 'none'}} onClick={onClose}>
                 <div className="modal-dialog" onClick={(event) => event.stopPropagation()}>
                     <div className="modal-content">
-                        <div className="modal-header">
+                        <div className="modal-header d-flex align-items-center">
                             <h1 className="modal-tiytle fs-5">{title}</h1>
+                            <button className="ms-auto border-0" onClick={onClose}>x</button>
                         </div>
                         {children}
                     </div>
                 </div>
             </div>
         </>
-
     );};
 
 export default Modal;
